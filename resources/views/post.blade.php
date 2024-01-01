378380cc5af5927c6f3acbceab7d618f57282a29
jellywiz <muzaffer.dler@gmail.com>
2024-01-01 17:05:09 +0300

    first commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,13 @@
+<x-layout>
+    <article>
+        <h1>
+            {{$post->title}}
+        </h1>
+        <p>
+            {!! $post->body !!}
+        </p>
+    </article>
+    <a href="/">Go Back</a>
+</x-layout>
+
+
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,15 @@
+<x-layout>
+    @foreach($posts as $post)
+        <article>
+            <h1>
+                <a href="/posts/{{$post->slug}}"> {{$post->title}}</a>
+            </h1>
+            <p>
+                {{$post->excerpt}}
+            </p>
+        </article>
+    @endforeach
+</x-layout>
+
+
+
